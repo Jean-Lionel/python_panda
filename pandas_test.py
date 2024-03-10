9f20289f40aeb3bc3e955fcdde050398cafe265d
Jean-Lionel <nijeanlionel@gmailcom>
2024-03-10 06:01:20 +0200

    lire les collonne personnalise

--- pandas_test.py
+++ pandas_test.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 if __name__ == '__main__':
-    file_2023 = pd.read_excel("excel/test.xlsx")
+    file_2023 = pd.read_excel("excel/test.xlsx", usecols="A:B,D", sheet_name="2023")
     print(file_2023)
     file_2024 = pd.read_excel("excel/test.xlsx", sheet_name="2024")
     print("==============================================================")
